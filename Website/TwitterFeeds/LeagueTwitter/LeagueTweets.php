8d5867191b4694763a644376ed402571884004c4
Ralph Babunga <kongwaralph@yahoo.com>
2018-10-13 19:51:23 -0400

    TwitterAPI Commit for each game
    I've put into the files, multiple changes to the twitter api as well as access for each game's esports twitter account.

---
+++ Website/TwitterFeeds/LeagueTwitter/LeagueTweets.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    
+        <script type="text/javascript" src="LeagueTweets.js"></script>
+        <link href="../../css/Tweets.css" rel="stylesheet" type="text/css" />
+        <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+    
+    </head>
+    
+    <body>
+    
+        <div id="twitter-feed"></div>
+    
+    </body>
+    
+</html>
